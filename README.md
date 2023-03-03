67f7a29d2abef89e6d163fe4462a09685a05e842
Nathan Wang <nathan.r.wang@gmail.com>
2023-03-03 18:55:41 -0500

    add basic yjs server

---
+++ README.md
@@ -0,0 +1,3 @@
+(WIP) yjs websocket server for cpinitiative/ide
+
+`HOST=localhost PORT=1234 YPERSISTENCE=ypersistence node server.js` or something
---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import * as dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(__dirname, ".env"),
+});
+
+import * as WebSocket from "ws";
+import * as http from "http";
+const wss = new WebSocket.Server({ noServer: true });
+const setupWSConnection = require("./utils.js").setupWSConnection;
+
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 1234;
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("okay");
+});
+
+wss.on("connection", setupWSConnection);
+
+server.on("upgrade", (request, socket, head) => {
+  // You may check auth of request here..
+  // See https://github.com/websockets/ws#client-authentication
+  /**
+   * @param {any} ws
+   */
+  const handleAuth = (ws) => {
+    wss.emit("connection", ws, request);
+  };
+  wss.handleUpgrade(request, socket, head, handleAuth);
+});
+
+// @ts-ignore later issue smh
+server.listen(port, host, () => {
+  console.log(`running at '${host}' on port ${port}`);
+});
