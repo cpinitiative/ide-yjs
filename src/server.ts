557b84c74950efeb20d85ce3e096c5df30df1ea7
Nathan Wang <nathan.r.wang@gmail.com>
2023-03-03 20:11:17 -0500

    setup https server

--- src/server.ts
+++ src/server.ts
@@ -7,19 +7,61 @@ dotenv.config({
 
 import * as WebSocket from "ws";
 import * as http from "http";
+import * as https from "https";
+import * as fs from "fs";
 const wss = new WebSocket.Server({ noServer: true });
 const setupWSConnection = require("./utils.js").setupWSConnection;
 
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 1234;
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("okay");
-});
+let server;
+if (process.env.NODE_ENV === "production") {
+  server = https.createServer();
+
+  const readCertsSync = () => {
+    return {
+      cert: fs.readFileSync(
+        "/etc/letsencrypt/live/yjs.usaco.guide/fullchain.pem",
+        "utf8"
+      ),
+      key: fs.readFileSync(
+        "/etc/letsencrypt/live/yjs.usaco.guide/privkey.pem",
+        "utf8"
+      ),
+    };
+  };
+
+  server = https.createServer(readCertsSync(), (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("okay");
+  });
+  server.listen(443, () => {
+    console.log(
+      "https server running on :443. Note that when NODE_ENV is prod, HOST and PORT are ignored"
+    );
+  });
+
+  let waitForCertAndFullChainToGetUpdatedTooTimeout;
+  fs.watch("/etc/letsencrypt/live/yjs.usaco.guide/fullchain.pem", () => {
+    clearTimeout(waitForCertAndFullChainToGetUpdatedTooTimeout);
+    waitForCertAndFullChainToGetUpdatedTooTimeout = setTimeout(() => {
+      server.setSecureContext(readCertsSync());
+    }, 1000);
+  });
+} else {
+  server = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("okay");
+  });
+
+  // @ts-ignore later issue smh
+  server.listen(port, host, () => {
+    console.log(`running at '${host}' on port ${port}`);
+  });
+}
 
 wss.on("connection", setupWSConnection);
-
 server.on("upgrade", (request, socket, head) => {
   // You may check auth of request here..
   // See https://github.com/websockets/ws#client-authentication
@@ -30,9 +72,4 @@ server.on("upgrade", (request, socket, head) => {
     wss.emit("connection", ws, request);
   };
   wss.handleUpgrade(request, socket, head, handleAuth);
-});
-
-// @ts-ignore later issue smh
-server.listen(port, host, () => {
-  console.log(`running at '${host}' on port ${port}`);
 });
